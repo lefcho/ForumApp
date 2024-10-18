69396cddb2340ec25fd835f2f619e16f09c2a19e
ivand <ivan.d.kostov@gmail.com>
2024-10-19 00:13:38 +0300

    added: pagination to dashboard

--- ForumApp/posts/views.py
+++ ForumApp/posts/views.py
@@ -69,6 +69,7 @@ class DashboardView(ListView, FormView):
     context_object_name = 'posts'
     form_class = SearchForm
     success_url = reverse_lazy('dashboard')
+    paginate_by = 2
 
     def get_queryset(self):
         queryset = self.model.objects.all()
